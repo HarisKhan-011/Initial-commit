d7b35d455780dc324b882fdfc8ff59b5633fab45
e-commerce <aharis0342@gmail.coml>
2024-12-04 00:07:30 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { Approutes } from './Routes/Approutes';
 import Navbar from './Components/Navbar';
 import { Footer } from './Components/Footer';
 import { CartProvider } from './CardContext/CardContext'; // Ensure correct path
-import Cardpage from './Pages/Cardpage';
+// import Cardpage from './Pages/Cardpage';
 
 function App() {
   const [show,setshow]=useState(false)
   return (
-    
+
     <CartProvider >
       <BrowserRouter>
         <Navbar  />
